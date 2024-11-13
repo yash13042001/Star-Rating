f431c578422d0c9e50d0d3c6e5446271ee0c3d81
Yash <shuklayash1304@gmail.com>
2024-11-13 15:22:51 +0530

    Star Rating Component Implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Rating</h1>
+      <div>
+        {[1, 2, 3, 4, 5].map((num) => (
+          <button
+            onClick={() => setRating(num)}
+            onMouseOver={() => setHover(num)}
+            onMouseLeave={() => setHover(rating)}
+          >
+            <span
+              className={`star ${
+                num <= ((rating && hover) || hover) ? "on" : "off"
+              }`}
+            >
+              &#9733;
+            </span>
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
